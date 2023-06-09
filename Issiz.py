10211146a2cf70f01efbc29f30e0ce133c1f336c
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:28:58 +0300

    Update Issiz.py

--- Issiz.py
+++ Issiz.py
@@ -1,3 +1,12 @@
 from Insan import Insan
 
 class Issiz(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk,tecrübe):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__gecmis_tecrube= tecrübe
+
+    def get_gecmis_tecrube(self):
+        return self.__gecmis_tecrube
+
+    def set_gecmis_tecrube(self, gecmis_tecrube):
+        self.__gecmis_tecrube = gecmis_tecrube
