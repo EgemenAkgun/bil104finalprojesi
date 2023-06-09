f40be5b5b400e77696c84fe9e92d04421ca8f99f
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:28:31 +0300

    Update Insan.py

--- Insan.py
+++ Insan.py
@@ -1,1 +1,12 @@
 class Insan:
+    def __init__(self,tc_no,ad,soyad,yas,cinsiyet,uyruk):
+        self.__tc_no = tc_no
+        self.__ad = ad
+        self.__soyad= soyad
+        self.__yas = yas
+        self.__cinsiyet = cinsiyet
+        self.__uyruk = uyruk
+
+#str metodu
+def __str__(self):
+    return f"Ad:{self.__name},Soyad:{self.__soyad},Yas:{self.__yas},Tc no:{self.__tc_no},Cinsiyet:{self.__cinsiyet},Uyruk:{self.__uyruk}"
