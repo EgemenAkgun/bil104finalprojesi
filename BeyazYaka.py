fc0aac63b2f0cce2e052f745453a8ca5431eb315
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:29:24 +0300

    Create BeyazYaka.py

---
+++ BeyazYaka.py
@@ -0,0 +1,7 @@
+from Calisan import Calisan
+
+
+class BeyazYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
+        self.__tesvik_primi = self.__kontrol_tesvik_primi(tesvik_primi)
