2a92f697ceaee6ae06f78fe960ff2339e580a302
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:29:33 +0300

    Update BeyazYaka.py

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -21,3 +21,16 @@ class BeyazYaka(Calisan):
     def set_tesvik_primi(self, tesvik_primi):
         self.__tesvik_primi = self.__kontrol_tesvik_primi(tesvik_primi)
 
+# zam miktarını hesaplayan fonksiyon
+    def zam_hakki(self):
+        if self.get_tecrube() < 2:
+            return self.get_tesvik_primi()
+        elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
+            return (self.get_maas() % self.get_tecrube()) * 5 + self.get_tesvik_primi()
+        elif self.get_tecrube() > 4 and self.get_maas() < 25000:
+            return (self.get_maas() % self.get_tecrube()) * 4 + self.get_tesvik_primi()
+        else:
+            return self.get_maas()
+
+    def __str__(self):
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrube: {self.get_tecrube()}\nYeni Maas: {self.zam_hakki() + self.get_maas()}\n"
