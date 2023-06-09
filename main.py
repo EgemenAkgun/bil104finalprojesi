3e8d0343d954345c071f8eab4382ea8bf17befd2
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:29:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from Insan import Insan
+from Issiz import Issiz
+from Calisan import Calisan
+from MaviYaka import MaviYaka
+from BeyazYaka import BeyazYaka
+
+# İnsan sınıfı için 2 nesne oluşturulması ve bilgilerin yazdırılması
