7cf932035d5e839a44e432eda4a17c02ea864bed
ozgetuzunn <128844924+EgemenAkgun@users.noreply.github.com>
2023-06-09 18:29:19 +0300

    Update MaviYaka.py

--- MaviYaka.py
+++ MaviYaka.py
@@ -21,3 +21,16 @@ class MaviYaka(Calisan):
     def set_yipranma_payi(self, yipranma_payi):
         self.__yipranma_payi = self.__kontrol_yipranma_payi(yipranma_payi)
 
+# zam oranını hesaplayan fonksiyon
+    def zam_hakki(self):
+        if self.get_tecrube() < 2:
+            return self.get_yipranma_payi() * 10
+        elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
+            return (self.get_maas() % self.get_tecrube()) / 2 + (self.get_yipranma_payi() * 10)
+        elif self.get_tecrube() > 4 and self.get_maas() < 25000:
+            return (self.get_maas() % self.get_tecrube()) / 3 + (self.get_yipranma_payi() * 10)
+        else:
+            return self.get_maas()
+
+    def __str__(self):
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrube: {self.get_tecrube()}\nYeni Maas: {self.zam_hakki()*1/100*self.get_maas() + self.get_maas()}\n"
